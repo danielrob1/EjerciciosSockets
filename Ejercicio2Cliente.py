7f0ff713a6fc38a4804e8daf2792c4ce56df141a
danielrob1 <daniel.robles.camazon@iesjulianmarias.es>
2024-12-17 12:44:54 +0100

    Initial commit

---
+++ Ejercicio2Cliente.py
@@ -0,0 +1,26 @@
+import socket
+
+def cliente_tcp():
+    # Crear socket
+    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c.connect(("127.0.0.1", 5000))
+    print("[CLIENTE] Conectado al servidor.")
+
+    # Recibir mensaje del servidor
+    #msg = c.recv(1024)
+    #print("[CLIENTE] Mensaje del servidor:", msg.decode())
+
+    # Enviar una respuesta
+    while(True):
+        respuesta = input()
+        c.send(respuesta.encode())
+
+    #msg=c.recv(1024)
+    #print("[CLIENTE] Mensaje del servidor:", msg.decode())
+
+
+    c.close()
+    print("[CLIENTE] Conexión cerrada.")
+
+# Ejecuta esta celda en otro entorno después de haber lanzado el servidor.
+cliente_tcp()
